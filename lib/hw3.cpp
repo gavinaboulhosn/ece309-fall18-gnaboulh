a6b6af3fe196a25a6456f1bfd156f6ec2607ff2a
Gavin Aboulhosn <gavinaboulhosn97@gmail.com>
2018-09-10 01:45:23 -0400

    forgot to add new changes

--- lib/hw3.cpp
+++ lib/hw3.cpp
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 
-class Node 
-{
-    private:
-        Item item;
-        Node *next;
-    public:
-        Node(Item a)
-        {
-            item = a;
-            next = NULL;
-        }
-        void setNext(Node *n) {next = n;}
-        Node* getNext() {return next;}
-        Item getItem() {return item;}
-        
-};
+using namespace std;
 
 class Item
 {
@@ -29,12 +14,113 @@ class Item
 };
 
 
+class ListArray 
+{
+    private:
+        int items;
+        int size;
+        Item* array;
+    public:
+        ListArray();
+        ListArray(int initSize);
+        ~ListArray();
+        void push_back(Item str);
+        Item& get(int n);
+        int length();
+        void remove_front();
+        void print();
+
+};
+
+void ListArray::push_back(Item str)
+{
+    if(size < items+1)
+    {
+        int newSize = size*2;
+        Item* newArr = new Item[newSize];
+        memcpy(newArr, array, size*sizeof(Item));
+        size = newSize;
+        delete[] array;
+        array = newArr;
+    }
+    array[items] = str;
+    items += 1;
+}
+
+Item& ListArray::get(int n)
+{
+    return array[n];
+}
+
+int ListArray::length()
+{
+    return items;
+}
+
+void ListArray::remove_front()
+{
+    
+    for(int i = 0; i < items-1; i++)
+    {
+        array[i]=array[i+1];
+
+    }
+    array[items-1] = "";
+    items -=1;
+    
+    
+}
+
+ListArray::ListArray()
+{
+    ListArray(5);
+}
+
+ListArray::ListArray(int initSize)
+{
+    size = initSize;
+    array = new Item[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i].str = NULL;
+    }
+    items = 0;
+}
 
 
+ListArray::~ListArray()
+{
+    cout << "deleted" << endl;
+    delete[] array;
+}
+
+void ListArray::print()
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i].str << endl;
+    }
+}
 
 
 int main(int argc, char const *argv[])
 {
-    
+    ListArray test = ListArray(1);
+    Item a = Item("a");
+    Item b = Item("b");
+    Item c = Item("c");
+    Item d = Item("d");
+    Item e = Item("e");
+    test.push_back(a);
+    test.push_back(b);
+    test.push_back(c);
+    test.push_back(d);
+    test.push_back(e);
+    cout << endl << test.length() << endl << endl;
+    test.print();
+    test.remove_front();
+    cout << endl << test.length()<< endl << endl;
+    test.print();
+    cout << test.get(3).str << endl;
     return 0;
 }
